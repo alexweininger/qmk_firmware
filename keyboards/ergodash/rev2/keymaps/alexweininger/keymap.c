67add9af0f3bb7e596687a633522ed9d5b9f4708
alexweininger <alex.weininger@live.com>
2019-04-25 22:45:48 -0700

    mirror the shift key on both hands

--- keyboards/ergodash/rev2/keymaps/alexweininger/keymap.c
+++ keyboards/ergodash/rev2/keymaps/alexweininger/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_LBRC,                       KC_RBRC, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_HOME, \
     TAB_ARR, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , HOMEEND,                       KC_BSPC, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, \
     ARR    , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_LALT,                       CODE   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_ENT , \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_LEAD,         SHIFCAP, KC_LCTL, RSE,           KC_SPC, LOW    , SHIFCAP,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_LEAD,         KC_LCTL , SHIFCAP,  RSE,           KC_SPC, SHIFCAP, LOW  ,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
   ),
 
   /* Lower
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F11,  LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),LGUI(KC_4),LGUI(KC_5),   KC_UNDS,                        KC_PLUS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LCBR,                        KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F1, \
     KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TAB ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQT , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
+    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, LSFT(LCTL(KC_ENT)), \
     KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC ,LCTL(KC_BSPC),   KC_SPC,KC_ENT , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
 
@@ -155,8 +155,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_CODE] = LAYOUT(
     KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , TD(TD_UND),                    KC_EQL , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSLS, \
     KC_GRV , KC_Q   , KC_W   , KC_E   , CURSUP   , CPYUP   , KC_LBRC,                       KC_RBRC, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_HOME, \
-    TAB_ARR, KC_A   , KC_S   , KC_D   , KC_F1  , CPYDWN   , HOMEEND,                       KC_BSPC, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, \
-    ARR    , KC_Z   , KC_X   , KC_C   , CURSDWN   , KC_B   , KC_LALT,                       CODE   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_ENT , \
+    TAB_ARR, KC_A   , KC_S   , LCTL(LSFT(KC_K)), KC_F1  , CPYDWN   , HOMEEND,                       KC_BSPC, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, \
+    ARR    , KC_Z   , KC_X   , KC_C   , CURSDWN   , KC_B   , KC_LALT,                       CODE   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, LSFT(LCTL(KC_ENT)) , \
     KC_LCTL, KC_LGUI, KC_LALT, KC_LEAD,         SHIFCAP, KC_LCTL, RSE,           KC_SPC, LOW    , SHIFCAP,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
   ),
 
